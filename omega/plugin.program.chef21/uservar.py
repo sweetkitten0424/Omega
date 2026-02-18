1dae5cf99dc07011b82dbfec97378d9e14f1c4f7
sweetkitten0424 <lukestot0103@gmail.com>
2026-02-18 11:55:04 -0700

    Update uservar.py

--- omega/plugin.program.chef21/uservar.py
+++ omega/plugin.program.chef21/uservar.py
@@ -1,5 +1,5 @@
 '''#####-----Build File-----#####'''
-buildfile = '192.168.1.168/kodi/builds/build-addons/omega.xml'
+buildfile = 'http://192.168.1.168/kodi/builds/build-addons/omega.xml'
 
 '''#####-----Videos File-----#####'''
 videos_url = 'https://raw.githubusercontent.com/Buildtexts69/omegabuilds/main/videos.txt'
